74bf11102785ba9fa619aa34192772e3ba16491d
Kelsey Judson <kelseyjudson@gmail.com>
2014-01-27 12:01:32 +1300

    Replace #_test_adapter ... changed mind.

--- lib/lucid_shopify/testing/session.rb
+++ lib/lucid_shopify/testing/session.rb
@@ -1,8 +1,27 @@
 module LucidShopify::Testing
   class Session < LucidShopify::Session
 
-    def _test_adapter( connection )
-      connection.adapter( :test ) do |api|
+    def initialize( options = {} )
+      super( options )
+
+      connection.builder.tap do |f|
+        replace_adapter( f )
+      end
+    end
+
+    private
+
+    def _default_uri
+      'https://example.myshopify.com'
+    end
+
+    # Assumes the adapter is the last item on the middleware stack (as it
+    # should be).
+    #
+    def replace_adapter( f )
+      f.handlers.pop
+
+      f.adapter( :test ) do |api|
         LucidShopify::Testing::Connection.assign_stubs( api )
       end
     end
